32ae1a64fbd7c1fec1e2e475dc227db9f05e70cb
MohamedKhedrawy <medomoataz2hof@gmail.com>
2024-11-02 20:10:13 +0200

    designed landingpage and dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,20 +4,56 @@ import QuestionPost from "../components/QuestionPost.jsx";
 import ClearRepeated from "../components/ClearRepeated.jsx";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { logout, reset } from "../features/auth/authSlice.js";
-import { useEffect } from "react";
+import { getMyInfo, logout, reset } from "../features/auth/authSlice.js";
+import { useEffect, useState, useRef } from "react";
 
  
 
 const Dashboard = () => {
 
-  const {isSuccess, isError, isLoading, message} = 
+  const {isSuccess, isError, isLoading, message, userName} = 
   useSelector((state) => state.auth)
+
+  const typingTextRef = useRef(null);
+  const intervalRef = useRef(null);
+  const nameRef = useRef(userName)
 
   const dispatch = useDispatch();
   const navigate = useNavigate()
 
   const token = localStorage.getItem('userToken')
+
+  useEffect(() => {
+    dispatch(getMyInfo())
+  }, [dispatch])
+
+  useEffect(() => {
+    nameRef.current = userName;
+  }, [userName]);
+
+  useEffect(() => {
+    const text = `Welcome ${nameRef.current}, What do you want to do today?`;
+    const typingSpeed = 50;
+    const startingDelay = 500;
+    let index = 0;
+
+    const typingTimeout = setTimeout(() => {
+      intervalRef.current = setInterval(() => {
+        typingTextRef.current.style.fontFamily = '"Press Start 2P", system-ui';
+        if (index < text.length) {
+          typingTextRef.current.innerHTML = text.slice(0, index + 1);
+          index++;
+        } else {
+          typingTextRef.current.innerHTML = text;
+          clearInterval(intervalRef.current);
+        }
+      }, typingSpeed) 
+    }, startingDelay)
+
+    return () => {clearInterval(intervalRef.current);
+      clearTimeout(typingTimeout)
+    }
+  }, [userName]);
 
   useEffect(() => {
     if (isLoading) {
@@ -30,27 +66,52 @@ const Dashboard = () => {
     }
 
     if (isSuccess || !token) {
-       navigate('/login');
+      navigate('/login');
     }
     
     dispatch(reset());
 
  }, [isLoading, isError, isSuccess, message, dispatch, navigate])
 
+
   const handleLogout = () => {
     dispatch(logout());
   }
 
-  return <>
-    <h1>Dashboard</h1>
+  return <main className="homepage">
+    <h1 className="title">EVAL</h1>
+    <h3 className="welcome" ref={typingTextRef}></h3>
+    <div className="dashboard">
+      
+      <div className="panel">
+        <h3>Choose Test Parameters</h3>
+        <p>Select a course and difficulty level, then set the number of questions.</p>
+        <button className="btn">Start Test</button>
+      </div>
+      <div className="panel">
+        <h3>Post New Question</h3>
+        <p>Add a new question to the database, including answer choices.</p>
+        <button className="btn">Add Question</button>
+      </div>
+      <div className="panel">
+        <h3>Clear Repeated Questions</h3>
+        <p>Remove duplicate questions from the test pool.</p>
+        <button className="btn">Clear Questions</button>
+      </div>
+      <div className="panel">
+        <h3>See Your Mistakes</h3>
+        <p>View mistakes and track your progress over time.</p>
+        <button className="btn">View Mistakes</button>
+      </div>
+        
+      {/* <TestParams />
+      <QuestionPost />
+      <ClearRepeated />
+      <Mistakes /> */}
 
-    <TestParams />
-    <QuestionPost />
-    <Mistakes />
-    <ClearRepeated />
-
-    <button onClick={handleLogout}>Logout</button>
-  </>
+      {/* <button onClick={handleLogout}>Logout</button> */}
+    </div>
+  </main>
  }
 
  export default Dashboard;
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,12 +1,71 @@
+import React, { useEffect } from "react";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 
 const LandingPage = () => {
-    return <>
-        <h1>Landing Page</h1>
-        <Link to={'/login'}>Log in</Link>
-        <Link to={'/register'}>Register</Link>
-    </>
+
+    const videoRef = useRef(null);
+    const typingTextRef = useRef(null);
+    const intervalRef = useRef(null);
+
+    const handleVideoEnd = () => {
+        if (videoRef.current) {
+            videoRef.current.currentTime = 0;
+            videoRef.current.play();
+        }
+    }
+
+    useEffect(() => {
+        const text = "Wake up, the matrix has you ...";
+        const typingSpeed = 100;
+        const startingDelay = 1000;
+        let index = 0;
+
+        typingTextRef.current.innerHTML = ''
+    
+        // Define the typing effect
+        const typingTimeout = setTimeout(() => {
+          intervalRef.current = setInterval(() => {
+            typingTextRef.current.style.fontFamily = '"Press Start 2P", system-ui';
+            if (index < text.length) {
+              typingTextRef.current.innerHTML = text.slice(0, index + 1);
+              index++;
+            } else {
+              typingTextRef.current.innerHTML = text; // Final text without the cursor
+              clearInterval(intervalRef.current); // Stop typing when done
+            }
+          }, typingSpeed);
+        }, startingDelay)
+
+        return () => {clearInterval(intervalRef.current);
+          clearTimeout(typingTimeout);
+        } // Cleanup on unmount
+      }, []);
+
+    return <main className="landing-page">
+        <video ref={videoRef} autoPlay loop muted preload="auto" className="background-video" onEnded={handleVideoEnd}>
+            <source src='../../public/11584395-uhd_3840_2160_60fps.mp4' type="video/mp4" />
+      </video>
+        <div className="landing-page-content">
+            <h1 className="title">EVAL</h1>
+            <div className="hero"><span ref={typingTextRef}></span></div>
+            <div className="pill-container">
+            <Link to={'/register'} className="link">
+                <div className="pill" >
+                  <img src="../../public/images/red-pill-small.png" id="red" />
+                  <p className="coa" id="red-coa">Create your Account</p>
+                </div>
+              </Link>
+              <Link to={'/login'} className="link">
+                <div className="pill" >
+                  <img src="../../public/images/blue-pill-small2.png" id="blue" />
+                  <p className="coa" id="blue-coa">Log in</p>
+                </div>
+              </Link>
+            </div>
+        </div>
+    </main>
 }
 
 export default LandingPage;
